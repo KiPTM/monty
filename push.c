4b403d070504682f7770f5a521b6e3db9b85d283
KiPTM <mathewtuwei@gmail.com>
2023-12-15 04:17:53 -0800

    update

--- push.c
+++
@@ -1,6 +0,0 @@
-#include "monty.h"
-
-/* Function to push an element onto the stack */
-void f_push(stack_t **head, unsigned int number) {
-    addnode(head, number); /* Assuming addnode function adds a node to the stack with the given number */
-}
