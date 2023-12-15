69e0476b4bccdf6b584f2308f67e432b74c5e43e
KiPTM <mathewtuwei@gmail.com>
2023-12-14 23:19:11 -0800

    update

--- pint.c
+++ pint.c
@@ -3,7 +3,7 @@
 /* Function to print the top element in the stack */
 void f_pint(stack_t **head, unsigned int number) {
     if (*head == NULL) {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", number);
+        fprintf(stderr, "L%d: cannot pint, stack empty\n", number);
         exit(EXIT_FAILURE);
     }
     printf("%d\n", (*head)->n);
