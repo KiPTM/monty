f17d1c4dbcf1db707888b81e969b61d124709438
KiPTM <mathewtuwei@gmail.com>
2023-12-18 03:42:34 -0800

    pint

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ * pint - Prints the value at the top of the stack
+ * @stack: Double pointer to the head of the stack
+ * @line_number: Line number in the Monty byte code file
+ *
+ * Description: This function prints the value at the top of the stack,
+ * followed by a new line.
+ */
+void pint(stack_t **stack, unsigned int line_number) {
+    if (*stack == NULL) {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", (*stack)->n);
+}
